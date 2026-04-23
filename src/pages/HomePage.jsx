bb8d3249dbb7a9eef194b7049e048a73d3caa8b5
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-04-23 17:05:50 +0200

    added Aesthetic to Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-
+import { Link } from 'react-router-dom'
 
 export default function HomePage() {
 
@@ -18,12 +18,15 @@ export default function HomePage() {
             <div className="row">
                 {
                     movies.map((movie, index) => (
-                        <div key={index} className="col">
-                            <div className="card">
-                                <img src={`http://127.0.0.1:3010/${movie.image}`} className="card-img-top" alt={movie.title} />
-                                <div className="card-body">
+                        <div key={index} className="col g-3">
+                            <div className="card h-100 mx-auto text-center text-info bg-dark" style={{ width: "18rem" }}>
+                                <img src={`http://127.0.0.1:3010/${movie.image}`} className="card-img-top h-100" alt={movie.title} />
+                                <div className="card-body text-white">
                                     <h3 className="card-title">{movie.title}</h3>
-                                    <p className="card-text">{movie.director}, {movie.genre}, {movie.release_year}</p>
+                                    <div className="card-text"><span className="text-info"> Genre:</span> {movie.genre} </div>
+                                    <div className="card-text"><span className="text-info"> Year:</span> {movie.release_year} </div>
+                                    <div className="card-text"><span className="text-info"> Director:</span> {movie.director} </div>
+                                    <Link to={`/movies/${movie.id}`} className="text-white btn btn-outline-info px-3 py-1 border-2 mt-1">View details</Link>
                                 </div>
                             </div>
                         </div>
